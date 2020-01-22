29d3dc4368dd1f98a8383d3abb2546ce2a074499
unknown <kaustubhkrishna9031@gmail.com>
2020-01-22 20:58:42 +0530

    now

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+<div class="sk-cube-grid" v-if="loading_bool">
+    <div class="sk-cube sk-cube1"></div>
+    <div class="sk-cube sk-cube2"></div>
+    <div class="sk-cube sk-cube3"></div>
+    <div class="sk-cube sk-cube4"></div>
+    <div class="sk-cube sk-cube5"></div>
+    <div class="sk-cube sk-cube6"></div>
+    <div class="sk-cube sk-cube7"></div>
+    <div class="sk-cube sk-cube8"></div>
+    <div class="sk-cube sk-cube9"></div>
+  </div>
+  <div v-else>
       <div
         id="slider-banner-section"
         :style="{
@@ -569,6 +580,7 @@
       getFeaturedStartups: function() {
         this.$store.dispatch("getFeaturedStartups").then(res => {
           this.featuredList = res.data.reverse().splice(0, 6);
+          this.loading_bool = false;
         });
         this.getCategories();
       },
