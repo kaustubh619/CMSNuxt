9e8695743566a2cabc32a89272689c0ca32ca475
unknown <kaustubhkrishna9031@gmail.com>
2020-01-24 23:55:46 +0530

    now

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,14 @@
   </div>
 </template>
 <script>
-import Header from "@/components/header.vue";
-import Footer from "@/components/footer.vue";
-export default {
-  components: {
-    Header,
-    Footer
-  }
-};
+  import Header from "@/components/header.vue";
+  import Footer from "@/components/footer.vue";
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  };
 </script>
 <style>
 /* html {
--- pages/startup/featured_startups.vue
+++ pages/startup/featured_startups.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="sk-cube-grid" v-if="loading_bool">
-    <div class="sk-cube sk-cube1"></div>
-    <div class="sk-cube sk-cube2"></div>
-    <div class="sk-cube sk-cube3"></div>
-    <div class="sk-cube sk-cube4"></div>
-    <div class="sk-cube sk-cube5"></div>
-    <div class="sk-cube sk-cube6"></div>
-    <div class="sk-cube sk-cube7"></div>
-    <div class="sk-cube sk-cube8"></div>
-    <div class="sk-cube sk-cube9"></div>
+  <div v-if="loading_bool">
+    <div class="sk-cube-grid">
+      <div class="sk-cube sk-cube1"></div>
+      <div class="sk-cube sk-cube2"></div>
+      <div class="sk-cube sk-cube3"></div>
+      <div class="sk-cube sk-cube4"></div>
+      <div class="sk-cube sk-cube5"></div>
+      <div class="sk-cube sk-cube6"></div>
+      <div class="sk-cube sk-cube7"></div>
+      <div class="sk-cube sk-cube8"></div>
+      <div class="sk-cube sk-cube9"></div>
+    </div>
+    <div style="margin: 100px">
+      <p style="text-align: center">Loading, please wait ...</p>
+    </div>
   </div>
   <div v-else>
     <div id="breadcrum-inner-block">
@@ -139,12 +144,10 @@
     methods: {
       getFeaturedStartups: function() {
         this.$store.dispatch("getFeaturedStartups").then(res => {
-          console.log(res.data);
           this.featuredList = res.data.reverse();
           this.loading_bool = false;
           this.length = res.data.length;
         });
-        // this.getCategories();
       }
     }
   };
